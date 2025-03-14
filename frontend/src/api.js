1c4bd44980ef745674d2fbc61397fec853232eb6
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-03-14 15:49:27 -0600

    Added choreo URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
+const apiURL = '/choreo-apis/first-react-django-app/backend/v1';
+
 // Create an axios instance and set the base URL to the API URL from the .env file.
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL
+        ? import.meta.env.VITE_API_URL
+        : apiURL,
 });
 
 // Add an interceptor to the axios instance to add the Authorization header with the access token to each request.
